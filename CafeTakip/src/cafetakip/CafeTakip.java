cf0584ee3f91dcb5cf66af513fcd01805c4f7359
sawyersago <ce.mustafasahin@gmail.com>
2014-02-01 11:53:18 +0200

    alaaaa

---
+++ CafeTakip/src/cafetakip/CafeTakip.java
@@ -0,0 +1,17 @@
+package cafetakip;
+
+import ServerModel.*;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author yetishbey
+ */
+public class CafeTakip {
+    private ArrayList <Bilgisayar> bilgisayarlar;
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
